13d72b79179ed1267cdadf8fabe0b44c1d403255
Rishabh Sinha <rishabhsinha2704@gmail.com>
2021-01-02 21:53:03 +0530

    First code in python

---
+++ helloworld.py
@@ -0,0 +1,42 @@
+#Comments in python ;-
+
+# Hello this is a single line comment in python
+
+#Multi line comment in Python:
+
+'''
+    rishabh Sinha
+'''
+
+# Printing Hello world in python:
+
+print("\nHellO WorlD\n")
+
+#Drawing a rectangular shape in python:
+
+print(" ---------------------------")
+print("|                           |")
+print("|                           |")
+print("|                           |")
+print("|                           |")
+print(" ---------------------------")
+
+#Command to go on to next line:
+
+print("\n")
+
+#Drawing a triangualar shape in python:
+
+print("            /|")
+print("           / |")
+print("          /  |")
+print("         /   |")
+print("        /    |")
+print("       /     |")
+print("      /      |")
+print("     /       |")
+print("    /        |")
+print("   /_________|")
+
+
+
